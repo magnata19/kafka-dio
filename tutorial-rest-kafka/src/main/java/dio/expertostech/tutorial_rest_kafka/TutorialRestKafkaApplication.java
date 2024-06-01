5c315b8f92d8d6d0a11a380768fa9e3929cba04d
magnata19 <dehpacifico@gmail.com>
2024-06-01 20:44:55 -0300

    first commit

---
+++ tutorial-rest-kafka/src/main/java/dio/expertostech/tutorial_rest_kafka/TutorialRestKafkaApplication.java
@@ -0,0 +1,13 @@
+package dio.expertostech.tutorial_rest_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRestKafkaApplication.class, args);
+	}
+
+}
